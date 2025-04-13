9e624bb7b7e4b098dd1612f8869f068f56f2f999
Shashank Verma <shashankvrma@gmail.com>
2025-04-13 03:47:19 -0400

    deployment

--- src/store.js
+++ src/store.js
@@ -1,7 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
-
-import cartReducer from './CartSlice';
  const store = configureStore({
     reducer: {
         cart: cartReducer,
